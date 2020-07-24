5120814dd48d5aa6812359c756d2fccf5f2e1391
kantasasakimaru <k_padbjw@icloud.com>
2020-07-24 15:13:41 +0900

    でーた受信周り

--- serial-connect.py
+++ serial-connect.py
@@ -1,11 +1,17 @@
+# coding: UTF-8
+
 import serial
+import csv
+import pandas as pd
 
 dev = "/dev/rfcomm0"
 rate = 9600
 ser = serial.Serial(dev, rate, timeout=10)
 def serial_send():
     data = "hello"
+    # data = "https://www.google.com"
     data += "\r\n"
+
     ser.write(data)
     print "============================"
 
@@ -19,6 +25,19 @@ def sentenceShaping(text):
     shaping_text = text.strip().decode('utf-8')
     return shaping_text
 
+def writeCsv(url):
+    with open('url.csv','a') as f:
+        writer = csv.writer(f)
+        writer.writerow([url])
+
+def readCsv():
+    with open("url.csv") as f:
+        for row in csv.reader(f):
+            print row
+
+# def updataCsv():
+
+
 beContinue = True
 
 beSend = True
@@ -27,4 +46,8 @@ serial_send()
 
 read_text = serial_read()
 read_text = sentenceShaping(read_text)
-print read_text
+print "取得したデータを書き込みます"
+# writeCsv(read_text)
+
+print "CSVファイルを読み込みます"
+readCsv()
