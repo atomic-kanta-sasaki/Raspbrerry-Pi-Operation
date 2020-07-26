4e34284d74ac07c01faa619681dbb42e42fd4e81
kantasasakimaru <k_padbjw@icloud.com>
2020-07-26 21:47:49 +0900

    RSSI を取得できるようになったが整形はできていない

--- getRSSI.py
+++ getRSSI.py
@@ -1,11 +1,11 @@
 # coding: UTF-8
 
 import subprocess as spc
-
-res = spc.check_output("getsi")
-lis = res.split()
-ss = lis.index('ESSID:"探したいSSID"')
-ss = ss - 2
-ra = lis[ss].split("=")
-RSSI = abs(int(ra[11]))
+res=spc.check_output("getsi")
+print res
+lis=res.split()
+ss=lis.index('ESSID:"探したいSSID"')
+ss=ss-2
+ra=lis[ss].split("=")
+RSSI=abs(int(ra[1]))
 print RSSI
