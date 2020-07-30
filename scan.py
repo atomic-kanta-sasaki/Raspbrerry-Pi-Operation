21cd570f5a71fa222548e47e5f16467fe6632ecb
kantasasakimaru <k_padbjw@icloud.com>
2020-07-30 14:53:05 +0900

    固定値を変数化

--- scan.py
+++ scan.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 import subprocess as spc
 
-# MACアドレスをリストに追加
-MAC_Address_List = ["D0:57:7B:20:46:ED", "00:28:F8:AA:6B:3E"]
+MAC_Address_1 = "D0:57:7B:20:46:ED"
+MAC_Address_2 = "00:28:F8:AA:6B:3E"
 
 # MACアドレスをKeyとする辞書を作成
-mydict = {"D0:57:7B:20:46:ED": "", "00:28:F8:AA:6B:3E": ""}
+mydict = {MAC_Address_1: "", MAC_Address_2: ""}
 
 # RSSIを取得するShellをたたく場所を作成
 res=spc.check_output("getsi")
@@ -13,9 +13,12 @@ res=spc.check_output("getsi")
 # 以下取得したデータを整形する
 res = res.split("\n")
 
-dict1_rssi = [row for row in res if 'D0:57:7B:20:46:ED' in row]
+dict1_rssi = [row for row in res if MAC_Address_1 in row]
 
-dict2_rssi = [row for row in res if '00:28:F8:AA:6B:3E' in row]
+dict2_rssi = [row for row in res if MAC_Address_2 in row]
+
+print dict1_rssi
+print dict2_rssi
 
 dict1_rssi = dict1_rssi[0].split(" ")
 dict2_rssi = dict2_rssi[0].split(" ")
@@ -23,8 +26,8 @@ dict2_rssi = dict2_rssi[0].split(" ")
 rssi_1 = dict1_rssi[6]
 rssi_2 = dict2_rssi[6]
 
-mydict["D0:57:7B:20:46:ED"] = rssi_1
-mydict["00:28:F8:AA:6B:3E"] = rssi_2
+mydict[MAC_Address_1] = rssi_1
+mydict[MAC_Address_2] = rssi_2
 
 print mydict
 
