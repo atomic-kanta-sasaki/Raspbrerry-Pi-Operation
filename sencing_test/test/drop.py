b7db3c5d496c40d944996e4cf426895b1e661ac9
kanta_sasaki <k_padbjw@icloud.com>
2020-08-24 18:45:00 +0900

    インタラクションの作成

---
+++ sencing_test/test/drop.py
@@ -0,0 +1,142 @@
+# coding: UTF-8
+
+import serial
+import csv
+import pandas as pd
+# import scan
+
+# MAC_Address_1に接続するためのシリアル
+dev = "/dev/rfcomm0"
+rate = 9600
+ser_1 = serial.Serial(dev, rate, timeout=10)
+
+# MAC_Address_2に接続するためのシリアル
+dev = "/dev/rfcomm1"
+rate = 9600
+ser_2 = serial.Serial(dev, rate, timeout=10)
+
+new_url_list = []
+# 使用するMACアドレスを変数化
+MAC_Address_1 = "1C:BF:C0:2B:52:D2"
+MAC_Address_2 = "00:28:F8:AA:6B:3E" 
+
+"""
+シリアル通信でURLORフラグを送信する
+
+@param flag or url
+"""
+def serial_send(ser, data):
+    ser.write(data)
+    print "============================"
+
+"""
+シリアル通信でデータを読み込む
+
+@return 受信データ
+
+"""
+def serial_read(ser):
+    res = ser.readline(10000)
+    print "---------------------------"
+    print res
+    return res
+
+"""
+シリアル通信で送られてきたデータを使用できる形に整形する
+
+@param シリアルデータ
+@return 整形後データ
+"""
+def sentenceShaping(text):
+    shaping_text = text.strip().decode('utf-8')
+    return shaping_text
+
+"""
+csvファイルにurlを書き込む
+
+@param url
+"""
+def writeCsv(url):
+    with open('url.csv','a') as f:
+        writer = csv.writer(f)
+        writer.writerow([url])
+
+"""
+送信すべきデータをcsvファイルから取得する
+
+@return  送信データ
+"""
+def readCsv():
+    with open("url.csv") as f:
+        for row in csv.reader(f):
+            return row[0]
+            break
+
+"""
+現在のCSVファイルをコンソールに表示する
+"""
+def printCsvContents():
+    with open("url.csv") as f:
+        for row in csv.reader(f):
+            print row
+
+"""
+csvファイルを更新する
+
+@param new_url_list
+"""
+def updataCsv(new_url_list):
+    with open("url.csv", 'w') as f:
+        writer = csv.writer(f)
+        for row in new_url_list:
+            writer.writerow(row)
+"""
+有効なURLが入っているindex番号を返す
+
+@return index
+"""
+def getValidUrlIndex():
+    with open("url.csv") as f:
+        for index, row in enumerate(csv.reader(f)):
+            if row == "end":
+                return index
+
+"""
+Drop後新しい更新用のURLリストを作成する
+
+@return new_url_list
+"""
+def createNewUrlList():
+    with open("url.csv") as f:
+        for row in csv.reader(f):
+            print "row"
+            print row
+            print row[0]
+            new_url_list.append(row)
+        
+        del new_url_list[0]
+        return new_url_list
+
+beContinue = True
+
+beSend = True
+
+def main():
+
+    data = readCsv()
+    print data
+    data += "\r\n"
+#    rssi_dict = scan.RSSI_Scan(MAC_Address_1, MAC_Address_2)
+    serial_send(ser_1, data)
+
+    # 送信したデータはcsvファイルから削除する
+    new_list = createNewUrlList()
+    updataCsv(new_list)
+    print "更新されたURLリスト"
+    print new_list
+
+    print "現在のCSVファイルの情報"
+    printCsvContents()
+
+if __name__ == '__main__':
+    main()
---
+++ sencing_test/test/scan.py
@@ -0,0 +1,84 @@
+# coding: utf-8
+import subprocess as spc
+import serial
+
+MAC_Address_1 = "1C:BF:C0:2B:52:D2"
+MAC_Address_2 = "00:28:F8:AA:6B:3E"
+
+dev = "/dev/rfcomm0"
+rate = 9600
+ser_1 = serial.Serial(dev, rate, timeout=10)
+
+dev = "/dev/rfcomm1"
+rate = 9600
+ser_2 = serial.Serial(dev, rate, timeout=10)
+
+"""
+特定のMACアドレスのRSSIを取得し辞書型で返す
+
+@param MAC_Address_1
+@param MAC_Address_2
+
+@return RSSI_dict
+"""
+def RSSI_Scan(MAC_Address_1, MAC_Address_2):
+
+    # MACアドレスをKeyとする辞書を作成
+    RSSI_dict = {MAC_Address_1: "", MAC_Address_2: ""}
+
+    # RSSIを取得するShellをたたく場所を作成
+    res=spc.check_output("getsi")
+
+    # 以下取得したデータを整形する
+    res = res.split("\n")
+
+    dict1_rssi = [row for row in res if MAC_Address_1 in row]
+
+    dict2_rssi = [row for row in res if MAC_Address_2 in row]
+
+    print dict1_rssi
+    print dict2_rssi
+
+    dict1_rssi = dict1_rssi[0].split(" ")
+    dict2_rssi = dict2_rssi[0].split(" ")
+
+    rssi_1 = dict1_rssi[6]
+    rssi_2 = dict2_rssi[6]
+
+    RSSI_dict[MAC_Address_1] = rssi_1
+    RSSI_dict[MAC_Address_2] = rssi_2
+
+    print RSSI_dict
+    return RSSI_dict
+
+"""
+キャリブレーションの際に必要なRSSI値の差を返却する
+
+@param RSSI_1
+@param RSSI_2
+
+@return difference
+"""
+def Calibration(RSSI_1, RSSI_2):
+    difference = RSSI_1 - RSSI_2
+    
+    return difference
+
+def serial_send(ser):
+    data = "hello"
+    data += "\r\n"
+    ser.write(data)
+    print "============================"
+
+rssi_dict = RSSI_Scan(MAC_Address_1, MAC_Address_2)
+
+if rssi_dict[MAC_Address_1] < rssi_dict[MAC_Address_2]:
+    serial_send(ser_1)
+else:
+    serial_send(ser_2)
+
+def main():
+    RSSI_Scan(MAC_Address_1, MAC_Address_2)
+
+if __name__ == '__main__':
+    main()
