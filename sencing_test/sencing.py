4b2dea8dfba282fa6e1a64e6c892b5d5c28f90b6
kanta_sasaki <k_padbjw@icloud.com>
2020-08-29 22:14:35 +0900

    DTW学習用データの追加

--- sencing_test/sencing.py
+++ sencing_test/sencing.py
@@ -47,26 +47,48 @@ GYRO_YOUT = 0x45        # Gyro Y-axis
 GYRO_ZOUT = 0x47        # Gyro Z-axis
 
 # Pickの学習用データを定義
-train_data_set_ax = pd.read_csv('../pick_train_data/pick_accel_x.csv', usecols=[0]).values.reshape(-1, 1)
-train_data_set_ay = pd.read_csv('../pick_train_data/pick_accel_y.csv', usecols=[0]).values.reshape(-1, 1)
-train_data_set_az = pd.read_csv('../pick_train_data/pick_accel_z.csv', usecols=[0]).values.reshape(-1, 1)
-train_data_set_gx = pd.read_csv('../pick_train_data/pick_gyro_x.csv', usecols=[3]).values.reshape(-1, 1)
-train_data_set_gy = pd.read_csv('../pick_train_data/pick_gyro_y.csv', usecols=[3]).values.reshape(-1, 1)
+train_data_set_ax = pd.read_csv('pick_train_data_2/pick_accel_x.csv', usecols=[2]).values.reshape(-1, 1)
+train_data_set_ay = pd.read_csv('pick_train_data_2/pick_accel_y.csv', usecols=[2]).values.reshape(-1, 1)
+train_data_set_az = pd.read_csv('pick_train_data_2/pick_accel_z.csv', usecols=[2]).values.reshape(-1, 1)
+train_data_set_gx = pd.read_csv('pick_train_data_2/pick_gyro_x.csv', usecols=[0]).values.reshape(-1, 1)
+
+train_data_set_gy = pd.read_csv('pick_train_data_2/pick_gyro_y.csv', usecols=[0]).values.reshape(-1, 1)
+train_data_set_gx_2 = pd.read_csv('pick_train_data_2/pick_gyro_x.csv', usecols=[1]).values.reshape(-1, 1)
+train_data_set_gy_2 = pd.read_csv('pick_train_data_2/pick_gyro_y.csv', usecols=[1]).values.reshape(-1, 1)
+train_data_set_gx_3 = pd.read_csv('pick_train_data_2/pick_gyro_x.csv', usecols=[2]).values.reshape(-1, 1)
+train_data_set_gy_3 = pd.read_csv('pick_train_data_2/pick_gyro_y.csv', usecols=[2]).values.reshape(-1, 1)
+train_data_set_gx_4 = pd.read_csv('pick_train_data_2/pick_gyro_x.csv', usecols=[3]).values.reshape(-1, 1)
+train_data_set_gy_4 = pd.read_csv('pick_train_data_2/pick_gyro_y.csv', usecols=[3]).values.reshape(-1, 1)
+train_data_set_gx_5 = pd.read_csv('pick_train_data_2/pick_gyro_x.csv', usecols=[4]).values.reshape(-1, 1)
+train_data_set_gy_5 = pd.read_csv('pick_train_data_2/pick_gyro_y.csv', usecols=[4]).values.reshape(-1, 1)
+
 
 # Dropの学習用データを定義
-drop_train_data_set_ax = pd.read_csv('../drop_train_data/drop_accel_x.csv', usecols=[0]).values.reshape(-1, 1)
-drop_train_data_set_ay = pd.read_csv('../drop_train_data/drop_accel_y.csv', usecols=[0]).values.reshape(-1, 1)
-drop_train_data_set_az = pd.read_csv('../drop_train_data/drop_accel_z.csv', usecols=[0]).values.reshape(-1, 1)
-drop_train_data_set_gx = pd.read_csv('../drop_train_data/drop_gyro_x.csv', usecols=[3]).values.reshape(-1, 1)
-drop_train_data_set_gy = pd.read_csv('../drop_train_data/drop_gyro_x.csv', usecols=[3]).values.reshape(-1, 1)
+drop_train_data_set_ax = pd.read_csv('drop_train_data/drop_accel_x.csv', usecols=[0]).values.reshape(-1, 1)
+drop_train_data_set_ay = pd.read_csv('drop_train_data/drop_accel_y.csv', usecols=[0]).values.reshape(-1, 1)
+drop_train_data_set_az = pd.read_csv('drop_train_data/drop_accel_z.csv', usecols=[0]).values.reshape(-1, 1)
+drop_train_data_set_gx = pd.read_csv('drop_train_data/drop_gyro_x.csv', usecols=[0]).values.reshape(-1, 1)
+drop_train_data_set_gx_2 = pd.read_csv('drop_train_data/drop_gyro_x.csv', usecols=[1]).values.reshape(-1, 1)
+drop_train_data_set_gx_3 = pd.read_csv('drop_train_data/drop_gyro_x.csv', usecols=[2]).values.reshape(-1, 1)
+drop_train_data_set_gx_4 = pd.read_csv('drop_train_data/drop_gyro_x.csv', usecols=[3]).values.reshape(-1, 1)
+drop_train_data_set_gx_5 = pd.read_csv('drop_train_data/drop_gyro_x.csv', usecols=[4]).values.reshape(-1, 1)
 
-
+drop_train_data_set_gy = pd.read_csv('drop_train_data/drop_gyro_x.csv', usecols=[0]).values.reshape(-1, 1)
+drop_train_data_set_gy_2 = pd.read_csv('drop_train_data/drop_gyro_x.csv', usecols=[1]).values.reshape(-1, 1)
+drop_train_data_set_gy_3 = pd.read_csv('drop_train_data/drop_gyro_x.csv', usecols=[2]).values.reshape(-1, 1)
+drop_train_data_set_gy_4 = pd.read_csv('drop_train_data/drop_gyro_x.csv', usecols=[3]).values.reshape(-1, 1)
+drop_train_data_set_gy_5 = pd.read_csv('drop_train_data/drop_gyro_x.csv', usecols=[4]).values.reshape(-1, 1)
 # テストデータを作成するための初期データを作成
 test_data_set_ax = np.arange(60).reshape(-1, 1)
 test_data_set_ay = np.arange(60).reshape(-1, 1)
 test_data_set_az = np.arange(60).reshape(-1, 1)
 test_data_set_gx = np.arange(60).reshape(-1, 1)
 test_data_set_gy = np.arange(60).reshape(-1, 1)
+
+pick_dtw_gx_list = []
+pick_dtw_gy_list = []
+drop_dtw_gx_list = []
+drop_dtw_gy_list = []
 
 """
 データ数120個の枠内に新しいデータを挿入し不要なデータをドロップさせる
@@ -150,7 +172,7 @@ pick動作を検出する
 @param 加速度、各加速度を用いたDTWの値
 """
 def check_pick_motion(dtw_ax_result, dtw_ay_result, dtw_az_result, dtw_gx_result, dtw_gy_result):
-    if dtw_ax_result < 320 and dtw_ay_result < 300 and dtw_az_result < 300 and dtw_gx_result < 800 and dtw_gy_result < 3000:
+    if 0.75 < accel_z and dtw_gx_result < 100000 and dtw_gy_result < 2000000:
         print ('pick')
         return 'pick'
 
@@ -159,7 +181,7 @@ Drop動作を検出する
 @param 加速度、各加速度を用いたDTWの値
 """
 def check_drop_motion(drop_dtw_ax_result, drop_dtw_ay_result, drop_dtw_az_result, drop_dtw_gx_result, drop_dtw_gy_result):
-    if drop_dtw_ax_result < 40 and drop_dtw_ay_result < 30 and drop_dtw_az_result < 30 and  drop_dtw_gx_result < 800 and drop_dtw_gy_result < 4200:
+    if 0.75 < accel_z and  drop_dtw_gx_result < 400000 and drop_dtw_gy_result < 2000000:
         print('drop')
         return 'drop'
 """
@@ -337,6 +359,8 @@ drop_count = 0
 loop_count = 1
 pick_motion_count = 0
 drop_motion_count = 0
+tt = 0
+
 while 1:
 #    print_sencing_data()
 #    print 'csvファイル書き込み'
@@ -362,7 +386,20 @@ while 1:
     pick_dtw_az_result = dtw.getDTW(train_data_set_az, test_data_set_az)
     pick_dtw_gx_result = dtw.getDTW(train_data_set_gx, test_data_set_gx)
     pick_dtw_gy_result = dtw.getDTW(train_data_set_gy, test_data_set_gy)
-    if check_pick_motion(pick_dtw_ax_result, pick_dtw_ay_result, pick_dtw_az_result, pick_dtw_gx_result, pick_dtw_gy_result) == 'pick':
+    pick_dtw_gx_result_1 = dtw.getDTW(train_data_set_gx_2, test_data_set_gx)
+    pick_dtw_gy_result_1 = dtw.getDTW(train_data_set_gy_2, test_data_set_gy)
+    pick_dtw_gx_result_2 = dtw.getDTW(train_data_set_gx_3, test_data_set_gx)
+    pick_dtw_gy_result_2 = dtw.getDTW(train_data_set_gy_3, test_data_set_gy)
+    pick_dtw_gx_result_3 = dtw.getDTW(train_data_set_gx_4, test_data_set_gx)
+    pick_dtw_gy_result_3 = dtw.getDTW(train_data_set_gy_4, test_data_set_gy)
+    pick_dtw_gx_result_4 = dtw.getDTW(train_data_set_gx_5, test_data_set_gx)
+    pick_dtw_gy_result_4 = dtw.getDTW(train_data_set_gy_5, test_data_set_gy)
+    
+    pick_dtw_gx_list.extend([pick_dtw_gx_result, pick_dtw_gx_result_1, pick_dtw_gx_result_2, pick_dtw_gx_result_3, pick_dtw_gx_result_4])
+    pick_dtw_gy_list.extend([pick_dtw_gy_result, pick_dtw_gy_result_1, pick_dtw_gy_result_2, pick_dtw_gy_result_3, pick_dtw_gy_result_4])
+    pick_dtw_gx_result = min(pick_dtw_gx_list) ** 2
+    pick_dtw_gy_result = min(pick_dtw_gy_list) ** 2
+    if tt > 80 and check_pick_motion(pick_dtw_ax_result, pick_dtw_ay_result, pick_dtw_az_result, pick_dtw_gx_result, pick_dtw_gy_result) == 'pick':
         count += 1
         print('=======================================================')
         print(count)
@@ -377,9 +414,24 @@ while 1:
         drop_dtw_ax_result = dtw.getDTW(drop_train_data_set_ax, test_data_set_ax)
         drop_dtw_ay_result = dtw.getDTW(drop_train_data_set_ay, test_data_set_ay)
         drop_dtw_az_result = dtw.getDTW(drop_train_data_set_az, test_data_set_az)
+
         drop_dtw_gx_result = dtw.getDTW(drop_train_data_set_gx, test_data_set_gx)
         drop_dtw_gy_result = dtw.getDTW(drop_train_data_set_gy, test_data_set_gy)
-        if check_drop_motion(drop_dtw_ax_result, drop_dtw_ay_result, drop_dtw_az_result, drop_dtw_gx_result, drop_dtw_gy_result) == 'drop':
+        drop_dtw_gx_result_1 = dtw.getDTW(drop_train_data_set_gx_2, test_data_set_gx)
+        drop_dtw_gy_result_1 = dtw.getDTW(drop_train_data_set_gy_2, test_data_set_gy)
+        drop_dtw_gx_result_2 = dtw.getDTW(drop_train_data_set_gx_3, test_data_set_gx)
+        drop_dtw_gy_result_2 = dtw.getDTW(drop_train_data_set_gy_3, test_data_set_gy)
+        drop_dtw_gx_result_3 = dtw.getDTW(drop_train_data_set_gx_4, test_data_set_gx)
+        drop_dtw_gy_result_3 = dtw.getDTW(drop_train_data_set_gy_4, test_data_set_gy)
+        drop_dtw_gx_result_4 = dtw.getDTW(drop_train_data_set_gx_5, test_data_set_gx)
+        drop_dtw_gy_result_4 = dtw.getDTW(drop_train_data_set_gy_5, test_data_set_gy)
+
+        drop_dtw_gx_list.extend([drop_dtw_gx_result, drop_dtw_gx_result_1, drop_dtw_gx_result_2, drop_dtw_gx_result_3, drop_dtw_gx_result_4])
+        drop_dtw_gy_list.extend([drop_dtw_gy_result, drop_dtw_gy_result_1, drop_dtw_gy_result_2, drop_dtw_gy_result_3, drop_dtw_gy_result_4])
+        drop_dtw_gx_result = min(drop_dtw_gx_list) ** 2
+        drop_dtw_gy_result = min(drop_dtw_gy_list) ** 2
+
+        if tt > 80  and check_drop_motion(drop_dtw_ax_result, drop_dtw_ay_result, drop_dtw_az_result, drop_dtw_gx_result, drop_dtw_gy_result) == 'drop':
             drop_count += 1
             print('======================================================')
             print(drop_count)
@@ -390,4 +442,8 @@ while 1:
                 loop_count += 1
     #print_pick_dtw_result(pick_dtw_ax_result, pick_dtw_ay_result, pick_dtw_az_result, pick_dtw_gx_result, pick_dtw_gy_result)
     #print_drop_dtw_result(drop_dtw_ax_result, drop_dtw_ay_result, drop_dtw_az_result, drop_dtw_gx_result, drop_dtw_gy_result)
-
+    pick_dtw_gx_list = []
+    pick_dtw_gy_list = []
+    drop_dtw_gx_list = []
+    drop_dtw_gy_list = []
+    tt += 1
