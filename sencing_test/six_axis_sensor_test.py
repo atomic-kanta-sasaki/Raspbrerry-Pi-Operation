26ba2e6e044611a2b234f348db063c7ae29f6f88
kantasasakimaru <k_padbjw@icloud.com>
2020-09-16 18:34:14 +0900

    学習データの作成

--- sencing_test/six_axis_sensor_test.py
+++ sencing_test/six_axis_sensor_test.py
@@ -61,8 +61,8 @@ drop_train_data_set_gy_5 = pd.read_csv('drop_train_data/drop_gyro_x.csv', usecol
 test_data_set_ax = np.arange(60).reshape(-1, 1)
 test_data_set_ay = np.arange(60).reshape(-1, 1)
 test_data_set_az = np.arange(60).reshape(-1, 1)
-test_data_set_gx = np.arange(60).reshape(-1, 1)
-test_data_set_gy = np.arange(60).reshape(-1, 1)
+test_data_set_gx = np.arange(50).reshape(-1, 1)
+test_data_set_gy = np.arange(50).reshape(-1, 1)
 
 pick_dtw_gx_list = []
 pick_dtw_gy_list = []
@@ -91,7 +91,7 @@ pick_dtw_gy_result_4 = []
 @return 生成した観測データセット
 """
 def remake_test_data_set(test_data_set, data):
-    new_data = np.insert(test_data_set, 60, data, axis=0)
+    new_data = np.insert(test_data_set, 50, data, axis=0)
     new_data = np.delete(new_data, 0, 0)
     return new_data
 
@@ -274,11 +274,17 @@ while 1:
     pick_dtw_gy_result = min(pick_dtw_gy_list)
     print(pick_dtw_gy_result)
     """
+    """
+    print("========================")
+    print(pick_dtw_gx_result)
+    print(pick_dtw_gy_result)
+    print("======================")
+    """
     if tt > 80 and check_pick_motion(pick_dtw_ax_result, pick_dtw_ay_result, pick_dtw_az_result, pick_dtw_gx_result, pick_dtw_gy_result) == 'pick':
         count += 1
         print('=======================================================')
         print(count)
-        dtw.getDTWPath(train_data_set_gy, test_data_set_gy)
+        #dtw.getDTWPath(train_data_set_gy, test_data_set_gy)
         print("======================================================")
         print(pick_dtw_ax_result, pick_dtw_ay_result, pick_dtw_az_result, pick_dtw_gx_result, pick_dtw_gy_result)
         
@@ -317,6 +323,10 @@ while 1:
         drop_dtw_gx_result = min(drop_dtw_gx_list) 
         drop_dtw_gy_result = min(drop_dtw_gy_list) 
         """
+        print("=======================")
+        print(drop_dtw_gx_result - pick_dtw_gx_result)
+        print(drop_dtw_gy_result - pick_dtw_gy_result)
+        print("=======================")
         if tt > 80  and check_drop_motion(drop_dtw_ax_result, drop_dtw_ay_result, drop_dtw_az_result, drop_dtw_gx_result, drop_dtw_gy_result) == 'drop':
             drop_count += 1
             print('======================================================')
