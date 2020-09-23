9277e4ac6e199930ec4381f82326f9a263485ca2
kantasasakimaru <k_padbjw@icloud.com>
2020-09-23 12:43:22 +0900

    新しいモーション認識ディレクトリの作成

--- sencing_test/six_axis_sensor_test.py
+++ sencing_test/six_axis_sensor_test.py
@@ -171,8 +171,7 @@ def operation_identification(diff_ax, diff_gx, diff_gy, ay, az):
     print(diff_ax, diff_gx, diff_gy, ay, az)
     if diff_ax > 20 and diff_gx > 2000 and diff_gy > -350 and 0.75 < az < 1.25:
         return 'pick'
-    elif diff_ax < -15 and diff_gx < -2000 and diff_gy < -300 and 0.75 < az < 1.25:
-
+    elif diff_ax < -15 and diff_gx < -2000 and diff_gy < -300 and 0.65 < az < 1.25:
         return 'drop'
 
 """
@@ -300,7 +299,7 @@ while 1:
         #print(pick_dtw_ax_result)
         #print(drop_dtw_ax_result)
         print('pick')
-        dtw.getDTWPath(train_data_set_gy, test_data_set_gy)
+        #dtw.getDTWPath(train_data_set_gy, test_data_set_gy)
         print("============================================================")
         
     else:
@@ -352,7 +351,7 @@ while 1:
             print('drop')
             #print(drop_dtw_gy_result)
             print('------------------------------------------------------')
-            dtw.getDTWPath(drop_train_data_set_gx, test_data_set_gx)
+            #dtw.getDTWPath(drop_train_data_set_gx, test_data_set_gx)
             #print_drop_dtw_result(drop_dtw_ax_result, drop_dtw_ay_result, drop_dtw_az_result, drop_dtw_gx_result, drop_dtw_gy_result)
 
 
@@ -367,4 +366,4 @@ while 1:
     drop_dtw_gy_list = []
     tt += 1
     elapsed_time = time.time()
-    #print(elapsed_time - sec)
+    print(elapsed_time - sec)
