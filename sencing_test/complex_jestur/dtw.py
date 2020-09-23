bb88f30b50b261221aefb37d7eab5828ad9a816a
kanta_sasaki <k_padbjw@icloud.com>
2020-09-23 17:54:07 +0900

    dtwの関数の修正

--- sencing_test/complex_jestur/dtw.py
+++ sencing_test/complex_jestur/dtw.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
 
-import numpy as np
 from scipy.spatial.distance import euclidean
 from fastdtw import fastdtw
 from matplotlib import pyplot as plt
-# 異なる2種類のデータを定義
-x = np.array([2, 0, 1, 1, 2, 4, 2, 1, 2, 0]).reshape(-1, 1)
-y = np.array([1, 1, 2, 4, 2, 1, 2, 0]).reshape(-1, 1)
-# DTWを計算
-distance, path = fastdtw(x, y, dist=euclidean)
-print("DTW距離:", distance)
-plt.plot(x, label='x')
-plt.plot(y, label='y')
-# 各点がどのように対応しているかを図示する
-for x_, y_ in path:
-  plt.plot([x_, y_], [x[x_], y[y_]], color='gray', linestyle='dotted', linewidth=1)
-plt.legend()
-plt.title('Our two temporal sequences')
-plt.show()
+import numpy as np
+
+def getDTW(train_data_set, test_data_set):
+    distance, path = fastdtw(train_data_set, test_data_set)
+#    print (distance)
+    return distance
+
+def getDTWPath(x, y):
+    distance, path = fastdtw(x, y, dist=euclidean)
+    plt.plot(x, label='x')
+    plt.plot(y, label='y')
+    for x_, y_ in path:
+        plt.plot([x_, y_], [x[x_], y[y_]], color='gray', linestyle='dotted', linewidth=1)
+    plt.legend()
+    plt.title('Our two temporal sequences')
+    plt.show()
+    return path
+
+def main():
+    getDTW(train_data_set, test_data_set)
+
+if __name__ == '__main__':
+    main()
