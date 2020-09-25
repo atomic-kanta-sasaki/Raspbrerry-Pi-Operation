30c7dc12c39566ea314ed4b27ec257fc5c9c1775
kanta_sasaki <k_padbjw@icloud.com>
2020-09-25 17:44:52 +0900

    学習データの作成

--- sencing_test/complex_jestur/motion_checker.py
+++ sencing_test/complex_jestur/motion_checker.py
@@ -24,18 +24,18 @@ GYRO_YOUT = 0x45        # Gyro Y-axis
 GYRO_ZOUT = 0x47        # Gyro Z-axis
 
 # Pickの学習用データを定義
-train_data_set_ax = pd.read_csv('pick_train_data_2/pick_accel_x.csv', usecols=[2]).values.reshape(-1, 1)
-train_data_set_ay = pd.read_csv('pick_train_data_2/pick_accel_y.csv', usecols=[2]).values.reshape(-1, 1)
-train_data_set_az = pd.read_csv('pick_train_data_2/pick_accel_z.csv', usecols=[2]).values.reshape(-1, 1)
-train_data_set_gx = pd.read_csv('pick_train_data_2/pick_gyro_x.csv', usecols=[2]).values.reshape(-1, 1)
-train_data_set_gy = pd.read_csv('pick_train_data_2/pick_gyro_y.csv', usecols=[2]).values.reshape(-1, 1)
+train_data_set_ax = pd.read_csv('pick_train_data_2/pick_accel_x.csv', usecols=[3]).values.reshape(-1, 1)
+train_data_set_ay = pd.read_csv('pick_train_data_2/pick_accel_y.csv', usecols=[3]).values.reshape(-1, 1)
+train_data_set_az = pd.read_csv('pick_train_data_2/pick_accel_z.csv', usecols=[3]).values.reshape(-1, 1)
+train_data_set_gx = pd.read_csv('pick_train_data_2/pick_gyro_x.csv', usecols=[3]).values.reshape(-1, 1)
+train_data_set_gy = pd.read_csv('pick_train_data_2/pick_gyro_y.csv', usecols=[3]).values.reshape(-1, 1)
 
 # Dropの学習用データを定義
-drop_train_data_set_ax = pd.read_csv('drop_train_data/drop_accel_x.csv', usecols=[2]).values.reshape(-1, 1)
-drop_train_data_set_ay = pd.read_csv('drop_train_data/drop_accel_y.csv', usecols=[2]).values.reshape(-1, 1)
-drop_train_data_set_az = pd.read_csv('drop_train_data/drop_accel_z.csv', usecols=[2]).values.reshape(-1, 1)
-drop_train_data_set_gx = pd.read_csv('drop_train_data/drop_gyro_x.csv', usecols=[2]).values.reshape(-1, 1)
-drop_train_data_set_gy = pd.read_csv('drop_train_data/drop_gyro_y.csv', usecols=[2]).values.reshape(-1, 1)
+drop_train_data_set_ax = pd.read_csv('drop_train_data/drop_accel_x.csv', usecols=[3]).values.reshape(-1, 1)
+drop_train_data_set_ay = pd.read_csv('drop_train_data/drop_accel_y.csv', usecols=[3]).values.reshape(-1, 1)
+drop_train_data_set_az = pd.read_csv('drop_train_data/drop_accel_z.csv', usecols=[3]).values.reshape(-1, 1)
+drop_train_data_set_gx = pd.read_csv('drop_train_data/drop_gyro_x.csv', usecols=[3]).values.reshape(-1, 1)
+drop_train_data_set_gy = pd.read_csv('drop_train_data/drop_gyro_y.csv', usecols=[3]).values.reshape(-1, 1)
 
 # テストデータを作成するための初期データを作成
 test_data_set_ax = np.arange(99, dtype=float).reshape(-1, 1)
